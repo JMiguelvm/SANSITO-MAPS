9bb503381bbe7858c4f7c899dfd6c5cff99efd67
MparrS <mserranoparra35@gmail.com>
2023-06-22 16:43:55 -0500

    Inicalización de ventana de busqueda

---
+++ Busqueda/index.js
@@ -0,0 +1,24 @@
+// Obtener elementos del DOM
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const searchResults = document.getElementById('search-results');
+const products = document.querySelectorAll('.product');
+
+// Función para filtrar productos
+function filterProducts() {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  // Iterar sobre los productos y mostrar u ocultar según el término de búsqueda
+  products.forEach((product) => {
+    const productName = product.querySelector('.product-name').textContent.toLowerCase();
+
+    if (productName.includes(searchTerm)) {
+      product.style.display = 'block';
+    } else {
+      product.style.display = 'none';
+    }
+  });
+}
+
+// Evento de tecla presionada en el campo de búsqueda
+searchInput.addEventListener('keyup', filterProducts);
