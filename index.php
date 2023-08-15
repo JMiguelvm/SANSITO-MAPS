f492a86a6d42b5739b46cfccce1e6f3564cf079b
L0G4Ntf <mylogan360@gmail.com>
2023-08-15 16:21:32 -0500

    Finalizado v1 del sistema de ingreso y registro

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="modules/css/heade.css">
+    <link rel="stylesheet" href="modules/css/header.css">
     <link rel="stylesheet" href="modules/css/products.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b414b30242.js" crossorigin="anonymous"></script>
--- modules/validar.php
+++ modules/validar.php
@@ -12,7 +12,7 @@ switch($option) {
 
         $user = $consulta->fetch_assoc();
 
-        if ($mail == $user['correo'] && $password == $user['contraseña']) {
+        if ($mail == $user['correo'] && $password == $user['contrasena']) {
             session_start();
             $_SESSION['usuario'] =  $user['ID_usuario'];
             header("Location: ../index.php");
@@ -20,6 +20,28 @@ switch($option) {
         }
     break;
     case 2: // Verificación Registro
+        $nombre = $_POST['regNombre'];
+        $apellido = $_POST['regApellido'];
+        $numero = $_POST['regContacto'];
+        $mail = $_POST['regCorreo'];
+        $contra = $_POST['regContraseña'];
+        $contra1 = $_POST['regConfirmContraseña'];
+
+        if ($contra == $contra1) {
+            $query = "INSERT INTO `usuarios`(`nombre`, `apellido`, `contacto`, `correo`, `contrasena`) VALUES ('$nombre','$apellido',$numero,'$mail','$contra')";
+            mysqli_query($conn, $query);
+            $consulta = mysqli_query($conn, "SELECT * FROM usuarios WHERE correo = '".$mail."'");
+
+            $user = $consulta->fetch_assoc();
+            session_start();
+            $_SESSION['usuario'] =  $user['ID_usuario'];
+            header("Location: ../index.php");
+            exit();
+        }
+        else {
+            echo "<script>alert('Las contraseñas deben ser iguales.');</script>";
+            header("Location: ../index.php");
+        }
     break;
     case 3: // Cerrar sesión
         session_start();
