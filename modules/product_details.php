1658d9e9e7d9773c0252b3cc6b6a34d6ab4f84cd
MparrS <mserranoparra35@gmail.com>
2023-08-29 13:33:43 -0500

    Correcion Productos

--- modules/product_details.php
+++ modules/product_details.php
@@ -3,94 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/pDetails.css">
+    <link rel="stylesheet" href="css/pDetails1.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b414b30242.js" crossorigin="anonymous"></script>
     <title>SANSITO-MAPS</title>
 </head>
 <body>
     <div>
-            <?php
-                include_once('sql/conexion.php');
-                include('header.php');
-                $productId = $_GET['productId'];
-                $sql = "SELECT `nombre_producto`, `imagen_producto`, `descripcion`, `precio`, `stock_disponible` FROM productos WHERE ID_producto=".$productId;
-                $result = $conn->query($sql);
-                // $comentario = "SELECT `comentario`, `puntuacion` FROM valoraciones WHERE ID_valoracion=".$valoracion;
-                // $resulta = $conn->query($comentario);
-                // $valoracion = $_GET['ID_valoracion'];
+        <?php
+            include_once('sql/conexion.php');
+            include('header.php');
+            $productId = $_GET['productId'];
+            $sql = "SELECT `nombre_producto`, `imagen_producto`, `descripcion`, `precio`, `stock_disponible`, `descuento` FROM productos WHERE ID_producto=".$productId;
+            $result = $conn->query($sql);
 
-
-            
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo '<div style="padding-top: 100px;">
-            <div id="product__container">
-                <div id="product__images">
-                    <div class="image__container">
-                        <img id="product__image1" onclick="changeImg(`'. $row["imagen_producto"] .'`)" class="product__image" src="'. $row["imagen_producto"] .'" width="80" height="80">
-                        <div class="overlay"></div>
-                    </div>
-                    <div class="image__container">
-                        <img id="product__image2" onclick="changeImg(`'. $row["imagen_producto"] .'`)" class="product__image" src="'. $row["imagen_producto"] .'" width="80" height="80">
-                        <div class="overlay"></div>
-                    </div>
-                    <div class="image__container">
-                        <img id="product__image3" onclick="changeImg(`'. $row["imagen_producto"] .'`)" class="product__image" src="'. $row["imagen_producto"] .'" width="80" height="80">
-                        <div class="overlay"></div>
-                    </div>
-                    <img id="product__mainImage" class="product__mainImage" src="'. $row["imagen_producto"] .'" width="400" height="400">
-                </div>          
-                <div id="product__attributes">
-                    <h2 id="product__name">'. $row["nombre_producto"] .'</h2>
-                    <div class="rating clearfix">
-                    <form action="" method="post">
-                        <input type="radio" class="pComentario" id="star5" name="rating" value="5">
-                        <label for="star5"></label>
-                        <input type="radio" class="pComentario" id="star4" name="rating" value="4">
-                        <label for="star4"></label>
-                        <input type="radio" class="pComentario" id="star3" name="rating" value="3">
-                        <label for="star3"></label>
-                        <input type="radio" class="pComentario" id="star2" name="rating" value="2">
-                        <label for="star2"></label>
-                        <input type="radio" class="pComentario" id="star1" name="rating" value="1">
-                        <label for="star1"></label>
-                        <div id="comentario">
-                            <textarea id="story" name="story" rows="5" cols="33">Ingrese su comentario..</textarea>
-                            <button type="submit">Enviar</button>
-                            <button type="button" id="cancelButton">Cancelar</button>
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '<div style="padding-top: 100px;">
+                    <div id="product__container">
+                        <div id="product__images">
+                            <div class="image__container">
+                                <img id="product__image1" onclick="changeImg(`'. $row["imagen_producto"] .'`)" class="product__image" src="'. $row["imagen_producto"] .'" width="80" height="80">
+                                <div class="overlay"></div>
+                            </div>
+                            <div class="image__container">
+                                <img id="product__image2" onclick="changeImg(`'. $row["imagen_producto"] .'`)" class="product__image" src="'. $row["imagen_producto"] .'" width="80" height="80">
+                                <div class="overlay"></div>
+                            </div>
+                            <div class="image__container">
+                                <img id="product__image3" onclick="changeImg(`'. $row["imagen_producto"] .'`)" class="product__image" src="'. $row["imagen_producto"] .'" width="80" height="80">
+                                <div class="overlay"></div>
+                            </div>
+                            <img id="product__mainImage" class="product__mainImage" src="'. $row["imagen_producto"] .'" width="400" height="400">
                         </div>
-                    </form>
-                    </div>
-                    <p id="product__description">'. $row["descripcion"] .'</p>';
-                    ?>
-                    +
-
-                    <?php        
-            
-            $precioNormalFormateado = number_format($row["precio"], 0, ',', '.');
-            $precioConDescuento = $row["precio"] * (1 - ($row["descuento"] / 100));
-            $precioConDescuentoFormateado = number_format($precioConDescuento, 0, ',', '.');
-            
-            echo '<style>
-                .strike {
-                    text-decoration: line-through;
-                }
-                
-                .discount-price {
-                    color: green;
-                }
-                
-                .discount-text {
-                    font-weight: bold;
-                    color: red;
-                }
-                
-                .discount-value {
-                    font-weight: bold;
-                    color: red;
-                }
-            </style>';
+                        <div id="product__attributes">
+                            <h2 id="product__name">'. $row["nombre_producto"] .'</h2>
+                            <div class="rating clearfix">
+                                <input type="radio" id="star5" name="rating" value="5">
+                                <label for="star5"></label>
+                                <input type="radio" id="star4" name="rating" value="4">
+                                <label for="star4"></label>
+                                <input type="radio" id="star3" name="rating" value="3">
+                                <label for="star3"></label>
+                                <input type="radio" id="star2" name="rating" value="2">
+                                <label for="star2"></label>
+                                <input type="radio" id="star1" name="rating" value="1">
+                                <label for="star1"></label>
+                            </div>
+                            <p id="product__description">'. $row["descripcion"] .'</p>';
 
                     $precioNormalFormateado = number_format($row["precio"], 0, ',', '.');
                     $descuento = isset($row["descuento"]) ? $row["descuento"] : 0;
@@ -161,19 +121,6 @@
             var mainImg = document.getElementById("product__mainImage");
             mainImg.setAttribute("src", newSrc);
         }
-
-        const comentarioTextArea = document.getElementById("comentario");
-        const comentarioCancelar = document.getElementById("cancelButton");
-        const comentarioEstrella = document.getElementsByClassName("pComentario");
-
-        for (let i = 0; i < comentarioEstrella.length; i++) {
-            comentarioEstrella[i].addEventListener("click", function () {
-                comentarioTextArea.style.display = "block";
-            });
-        }
-        comentarioCancelar.addEventListener("click", function () {
-            comentarioTextArea.style.display = "none";
-        });
     </script>
 </body>
 </html>
